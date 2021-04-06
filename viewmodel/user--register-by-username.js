b77a56982e9676a059c9fd8d0a142dff6d6886a3
nlpxdc <nlpx_dc@hotmail.com>
2021-04-07 00:14:50 +0800

    add user register by username page

--- viewmodel/user--register-by-username.js
+++ viewmodel/user--register-by-username.js
@@ -1,6 +1,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: ''
+        username: '',
+        password: ''
+    },
+    methods: {
+        handleRegisterTap() {
+            console.log('register tap');
+            this.userRegisterByUsername();
+        },
+        userRegisterByUsername() {
+            axios.post('/userLogin/registerByUsername', {
+                username: this.username,
+                password: this.password
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var data = response.data;
+                    localStorage['jimAccessToken'] = data.accessToken;
+                    alert('register succeed'+data.userId);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    alert(error.response.data.message);
+                });
+        }
     }
 });
