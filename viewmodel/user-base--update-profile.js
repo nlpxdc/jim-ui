74d54722257be37d9a3536b687d1ef407d87f5c0
nlpxdc <nlpx_dc@hotmail.com>
2021-05-01 22:29:12 +0800

    fix

--- viewmodel/user-base--update-profile.js
+++ viewmodel/user-base--update-profile.js
@@ -8,11 +8,25 @@ var app = new Vue({
     },
     mounted() {
         console.log('view mounted');
+        this.userBaseLoadProfile();
     },
     methods: {
         handleUpdateTap() {
             console.log('update tap');
             this.userBaseUpdateProfile();
+        },
+        userBaseLoadProfile() {
+            axios.post('/userBase/loadProfile')
+                .then(response => {
+                    console.log(response);
+                    var user = response.data;
+                    this.nickname = user.nickname;
+                    this.area = user.area;
+                })
+                .catch(error => {
+                    console.log(error);
+                    alert(error.response.data.message);
+                });
         },
         userBaseUpdateProfile() {
             axios.post('/userBase/updateProfile', {
