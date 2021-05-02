bd68fdfd4c2859d78e21e2c8a1a0c6f1bb2f021b
nlpxdc <nlpx_dc@hotmail.com>
2021-05-02 20:37:32 +0800

    add uxy agree friend

--- viewmodel/uxy--batch-get-new-friend.js
+++ viewmodel/uxy--batch-get-new-friend.js
@@ -10,11 +10,28 @@ var app = new Vue({
         this.uxyBatchGetNewFriend();
     },
     methods: {
+        handleAgreeTap(uxId) {
+            console.log('agree tap');
+            this.uxyAgreeFriend(uxId);
+        },
         uxyBatchGetNewFriend() {
             axios.post('/uxy/batchGetNewFriend')
                 .then(response => {
                     console.log(response);
                     this.newFriends = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                    alert(error.response.data.message);
+                });
+        },
+        uxyAgreeFriend(userId) {
+            axios.post('/uxy/agreeFriend', {
+                userId: userId
+            })
+                .then(response => {
+                    console.log(response);
+                    alert('succeed');
                 })
                 .catch(error => {
                     console.log(error);
